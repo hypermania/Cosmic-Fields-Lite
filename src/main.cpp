1d430d2cb725ac6c5126f8f2880b32de24b9e0d7
hypermania <sl135@rice.edu>
2025-05-29 00:20:03 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -533,8 +533,8 @@ void generate_ic_sp(void)
 
   
   // Set the directory for output.
-  const std::string dir = "output/SP_IC/";
-  // const std::string dir = "/media/hypermania/Drive_001/FreeStreamingULDM/SP_IC/";
+  // const std::string dir = "output/SP_IC/";
+  const std::string dir = "/media/hypermania/Drive_001/FreeStreamingULDM/SP_IC/";
   prepare_directory_for_output(dir);
 
   
